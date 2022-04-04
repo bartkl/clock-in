5955bbd673d5343cc3c59b979d6b7171e7e445ef
Bart Kleijngeld <bartkl@gmail.com>
2022-04-04 23:01:31 +0200

    Using tempfile for displaying the overview text

--- clock-in.py
+++ clock-in.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+import os
 import subprocess
+from tempfile import NamedTemporaryFile
 from enum import IntEnum
+from typing import Optional
 
 
 class Activity(IntEnum):
@@ -9,17 +12,26 @@ class Activity(IntEnum):
     SLACK = 1
 
 
-def run_clock_in_dialog() -> Activity:
-    cmd = """
+def generate_overview() -> str:
+    text_file = NamedTemporaryFile(delete=False).name
+    print(text_file)
+    with open("example-output.txt") as g, open(text_file, mode="w") as f:
+        f.write(g.read())
+
+    return text_file
+
+
+def run_clock_in_dialog(overview_file) -> Optional[Activity]:
+    cmd = f"""
         yad \
-            --title "Clocker" \
+            --title "Clock In" \
             --window-icon "appointment" \
             --width 340 \
             --height 340 \
             --center \
             --text-info \
             --fontname "Monospace 10" \
-            --filename example-output.txt \
+            --filename {overview_file} \
             --margins 12 \
             --buttons-layout center \
             --button Worked!emblem-default:0 \
@@ -28,9 +40,19 @@ def run_clock_in_dialog() -> Activity:
 
     completed_process = subprocess.run([cmd], shell=True)
 
-    return Activity(completed_process.returncode)
+    try:
+        return Activity(completed_process.returncode)
+    except ValueError:
+        return
+
+
+def main():
+    overview_file = generate_overview()
+    print(run_clock_in_dialog(overview_file))
+
+    os.unlink(overview_file)  # Remove tempfile.
 
 
 
 if __name__ == "__main__":
-    print(run_clock_in_dialog())
+    main()
