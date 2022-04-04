2f63b2d89d1ad3e6da9f59e073b08b93d7aced76
Bart Kleijngeld <bartkl@gmail.com>
2022-04-04 22:39:52 +0200

    Cleanup

--- clock-in.py
+++ clock-in.py
@@ -2,23 +2,28 @@
 
 import subprocess
 
-UI_CMD = """
-yad \
-	--title "Clocker" \
-	--window-icon "appointment" \
-	--width 340 \
-	--height 340 \
-	--center \
-	--text-info \
-	--fontname "Monospace 10" \
-	--filename example-output.txt \
-	--margins 12 \
-	--buttons-layout center \
-	--button Worked!emblem-default:0 \
-	--button Slacked!weather:1
-"""
+
+def run_clock_in_dialog():
+    cmd = """
+        yad \
+            --title "Clocker" \
+            --window-icon "appointment" \
+            --width 340 \
+            --height 340 \
+            --center \
+            --text-info \
+            --fontname "Monospace 10" \
+            --filename example-output.txt \
+            --margins 12 \
+            --buttons-layout center \
+            --button Worked!emblem-default:0 \
+            --button Slacked!weather:1
+    """
+    return subprocess.run([cmd], shell=True)
+
+
 
 
 if __name__ == "__main__":
-    completed_process = subprocess.run([UI_CMD], shell=True)
+    completed_process = run_clock_in_dialog()
     print(completed_process.returncode)
