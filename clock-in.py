234fc9ec5cf469c53c881678d3274056e36b5392
Bart Kleijngeld <bartkl@gmail.com>
2022-04-05 11:07:47 +0200

    Expander works somehow

--- clock-in.py
+++ clock-in.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import random
 import subprocess
 from tempfile import NamedTemporaryFile
 from enum import IntEnum
@@ -20,18 +21,72 @@ def generate_overview() -> str:
     return text_file
 
 
-def run_clock_in_dialog(overview_file) -> Optional[Activity]:
+def run_clock_in_dialog_org(overview_file) -> Optional[Activity]:
+    plug_id = random.randint(1024, 65536)
     cmd = f"""
         yad \
-            --title "Clock In" \
-            --window-icon "appointment" \
-            --width 340 \
-            --height 340 \
-            --center \
+            --plug {plug_id} \
+            --tabnum 1 \
+            --text Test \
+            #--title "Clock In" \
+            #--window-icon "appointment" \
+            #--width 340 \
+            #--height 340 \
+            #--center \
+            #--text-info \
+            #--fontname "Monospace 10" \
+            #--filename {overview_file} \
+            #--margins 12 \
+            #--buttons-layout center \
+            #--button Register!bookmark-new:0 \
+            #--button Calibrate!find-location-symbolic:1 \
+            &
+        yad \
+            --plug {plug_id} \
+            --tabnum 2 \
             --text-info \
             --fontname "Monospace 10" \
             --filename {overview_file} \
             --margins 12 \
+            &
+        yad \
+            --paned \
+            --key {plug_id} \
+            --width 340 \
+            --height 340
+    """
+
+    completed_process = subprocess.run([cmd], shell=True)
+
+    try:
+        return Activity(completed_process.returncode)
+    except ValueError:
+        return
+
+
+
+def run_clock_in_dialog(overview_file) -> Optional[Activity]:
+    plug_id = random.randint(1024, 65536)
+    cmd = f"""
+        yad \
+            --plug {plug_id} \
+            --tabnum 1 \
+            --text Test \
+            &
+        yad \
+            --plug {plug_id} \
+            --expander More \
+            --tabnum 2 \
+            --text-info \
+            --fontname "Monospace 10" \
+            --filename {overview_file} \
+            --margins 12 \
+            &
+        yad \
+            --paned \
+            --key {plug_id} \
+            --width 340 \
+            --height 340 \
             --buttons-layout center \
             --button Register!bookmark-new:0 \
             --button Calibrate!find-location-symbolic:1
