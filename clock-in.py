e7c8e6ffa41af090145ec188aaf315097c4cd929
Bart Kleijngeld <bartkl@gmail.com>
2022-04-05 12:07:21 +0200

    Very nice design and sensible output

--- clock-in.py
+++ clock-in.py
@@ -22,20 +22,37 @@ def generate_overview() -> str:
 
 
 def run_clock_in_dialog(overview_file) -> Optional[Activity]:
+    hours = '04:30'
+
+    worked = "Worked:"
+    left = "Left:\t"
+    surplus = "Surplus:"
+
+    left_or_surplus_today = surplus
+    left_or_surplus_week = left
+
+    info = f"""
+        <span size="large"><b>\tToday\n</b></span>
+        <span size="medium">\t\t{worked}\t\t\t07:28</span>
+        <span size="medium">\t\t{left_or_surplus_today}\t\t\t05:28</span>
+        \n
+        <span size="large"><b>\tWeek\n</b></span>
+        <span size="medium">\t\t\{worked}\t\t\t07:28</span>
+        <span size="medium">\t\t{left_or_surplus_week}\t\t\t05:28</span>
+        \n
+    """
+
     cmd = f"""
         yad \
             --title "Clock In" \
             --window-icon "appointment" \
             --width 340 \
-            --height 340 \
+            --height 240 \
             --center \
-            --text-info \
-            --fontname "Monospace 10" \
-            --filename {overview_file} \
-            --margins 12 \
+            --text '{info}' \
             --buttons-layout center \
-            --button Register!bookmark-new:0 \
-            --button Calibrate!find-location-symbolic:1 \
+            --button 'Add {hours}'!bookmark-new:0 \
+            --button "Didn't work"!find-location-symbolic:1 \
     """
 
     completed_process = subprocess.run([cmd], shell=True)
